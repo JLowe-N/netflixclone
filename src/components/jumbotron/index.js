830fc47995106dcc4eff038ac1313077e8991c5b
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-19 15:21:19 -0500

    Jumbotron component JSX set up

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Pass & destructure component children, direction for reusability/flexibility
+// Also use spread operator to allow developer to use API as they see fit
+// Spread operator will capture any additional components
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+    return (
+        <Item {...restProps}>
+            <Inner direction={direction}>{children}</Inner> {/* Where Image & Text will go */}
+        </Item>
+    )
+}
+
+// Key to Compound Components Design Pattern
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+}
