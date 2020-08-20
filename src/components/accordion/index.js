be09697a98b8ca3b9f18d0ccd4428060fd70365d
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-20 17:01:38 -0500

    Further accordion styling, set up its ToggleContext

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import { Container, Inner, Frame, Item, Title, Header, Body } from './styles/accordion';
 
+const ToggleContext = createContext();
 
 export default function Accordion({ children, ...restProps }) {
     return (
@@ -11,11 +12,17 @@ export default function Accordion({ children, ...restProps }) {
 }
 
 Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-    return <Frame {...restProps}>{children}</Frame>
+    return <Frame {...restProps}>{children}</Frame>;
 }
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
-    return <Item {...restProps}>{children}</Item>
+    const [toggleShow, setToggleShow] = useState(false);
+
+    return (
+        <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+            <Item {...restProps}>{children}</Item>
+        </ToggleContext.Provider>
+    )
 }
 
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
@@ -23,9 +30,24 @@ Accordion.Title = function AccordionTitle({ children, ...restProps }) {
 }
 
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
-    return <Header {...restProps}>{children}</Header>;
+    const { toggleShow, setToggleShow } = useContext(ToggleContext)
+
+    return (
+        <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
+            {children}
+            {toggleShow ?
+                (
+                    <img src="images/icons/close-slim.png" alt="Close" />
+                ) :
+                (
+                    <img src="/images/icons/add.png" alt="Open" />
+                )}
+        </Header>
+    )
 }
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
-    return <Body {...restProps}>{children}</Body>;
+    const { toggleShow } = useContext(ToggleContext);
+
+    return toggleShow ? <Body {...restProps}>{children}</Body> : null;
 }
--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -28,11 +28,14 @@ export const Title = styled.h1`
 
 export const Frame = styled.div`
     margin-bottom: 40px;
+    max-width: 1200px;
+
 `;
 
 export const Item = styled.div`
     color: white;
     margin-bottom: 10px;
+    max-width: 1200px;
 
     &:first-of-type {
         margin-top: 3em;
@@ -43,16 +46,33 @@ export const Header = styled.div`
     display: flex;
     justify-content: space-between;
     cursor: pointer;
+    box-sizing: border-box;
     font-size: 26px;
     font-weight: normal;
     background: #303030;
     margin-bottom: 1px;
     padding: 0.8em 1.2em 0.8em 1.2em;
     user-select: none;
+    width: 100%;
+
+    img {
+        filter: brightness(0) invert(1);
+        width: 24px;
+
+        @media (max-width: 600px) {
+            width: 16px;
+        }
+    }
+    
+    @media (max-width: 600px) {
+        font-size: 16px;
+    }
+
+    }
 `;
 
 export const Body = styled.div`
-    max-width: 1200px;
+    box-sizing: border-box;
     transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
     font-size: 26px;
     font-weight: normal;
@@ -61,6 +81,7 @@ export const Body = styled.div`
     padding: 0.8em 1.2em 0.8em 1.2em;
     user-select: none;
     align-items: center;
+    width: 100%;
 
     @media (max-width: 600px) {
         font-size: 16px;
