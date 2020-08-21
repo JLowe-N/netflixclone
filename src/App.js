691dd86293699def2641e012b31793d78c7bec9e
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-21 14:39:48 -0500

    Update Form Container design

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from './constants/routes'
 import './App.css';
-import Home from './pages/home'
-import SignIn from './pages/sign_in'
+import { Home, Signin } from './pages'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path={ROUTES.SIGN_IN}>
-          <SignIn />
+          <Signin />
         </Route>
         <Route path={ROUTES.SIGN_UP}>
           <p>I will be the sign up page</p>
--- src/containers/form.js
+++ src/containers/form.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from '../components';
+import { SIGN_UP } from '../constants/routes'
 
-export function FormContainer({children}){
+export function FormContainer({children}) {
+    const [error, setError] = useState(null)
+    const [emailAddress, setEmailAddress] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignin = (event) => {
+        event.preventDefault();
+
+        // call in here to firebase to auth the user
+        // if there's an error, populate the error state
+    }
+
+    console.log(emailAddress)
+
     return (
         <Form>
-            <Form.Title>My Form</Form.Title>
-            <Form.Text>Please log in:</Form.Text>
-            <Form.Input />
-            <Form.SubmitButton>Log In</Form.SubmitButton>
+            <Form.Title>Sign In</Form.Title>
+            {error && <Form.Error>{error}</Form.Error>}
+
+            <Form.Base onSubmit={handleSignin}>
+                <Form.Input 
+                placeholder="Email address"
+                value={emailAddress}
+                onChange={({target}) => setEmailAddress(target.value)}
+                />
+                <Form.Input
+                type="password" 
+                placeholder="Password"
+                value={password}
+                autoComplete="off"
+                onChange={({target}) => setPassword(target.value)}
+                />
+                <Form.SubmitButton disabled={false} type="submit">
+                   Sign In
+                </Form.SubmitButton>
+
+                <Form.Text>
+                    New to Netflix? <Form.Link to={SIGN_UP}>Sign up now.</Form.Link>
+                </Form.Text>
+                <Form.TextSmall>
+                    This page is protected by Google reCAPTCHA.
+                </Form.TextSmall>
+            </Form.Base>
         </Form>
     )
 }
