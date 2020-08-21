9c010aef25c08da2ca853154e83c2b21ef21f31e
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-21 14:17:01 -0500

    Extend compound Form component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import { Container, Title, Text, Input, SubmitButton, Error } from './styles/form'
+import { 
+    Container,
+    Base,
+    Title, 
+    Text,
+    TextSmall, 
+    Input, 
+    SubmitButton,
+    Link, 
+    Error 
+} from './styles/form';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 export default function Form({children, ...restProps}) {
-    return <Container {...restProps}>{children}</Container>
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Form.Base = function FormBase({children, ...restProps}) {
+    return <Base {...restProps}>{children}</Base>;
 }
 
 Form.Title = function FormTitle({children, ...restProps}) {
-    return <Title {...restProps}>{children}</Title>
+    return <Title {...restProps}>{children}</Title>;
 }
 
 Form.Text = function FormText({children, ...restProps}) {
-    return <Text {...restProps}>{children}</Text>
+    return <Text {...restProps}>{children}</Text>;
 }
 
-Form.Input = function FormInput({...restProps}) {
-    return <Input {...restProps} />
+Form.TextSmall = function FormTextSmall({children, ...restProps}) {
+    return <TextSmall {...restProps}>{children}</TextSmall>;
+}
+
+Form.Input = function FormInput({children, ...restProps}) {
+    return <Input {...restProps}>{children}</Input>;
 }
 
 Form.SubmitButton = function FormSubmitButton({ children, ...restProps}) {
-    return <SubmitButton {...restProps}>{children}</SubmitButton>
+    return <SubmitButton {...restProps}>{children}</SubmitButton>;
+}
+
+Form.Link = function FormLink({to, children, ...restProps}) {
+    return (
+        <ReactRouterLink to={to}>
+            <Link {...restProps}>{children}</Link>
+        </ReactRouterLink>
+    )
 }
 
 Form.Error = function FormError({children, ...restProps}) {
--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
+import { Link as ReactRouterLink } from 'react-router-dom'
 
 export const Container = styled.section``;
 
+export const Base = styled.form``;
+
 export const Title = styled.h1``;
 
-export const Text = styled.p``;
+export const Text = styled.h1``;
+
+export const TextSmall = styled.h2``;
 
 export const Input = styled.input``;
 
 export const SubmitButton = styled.button``;
 
-export const Error = styled.p``;
+export const Link = styled(ReactRouterLink)``;
+
+export const Error = styled.div``;
