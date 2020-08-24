c489bae6e99ae45d5b4d4fb7e8cc3b48d8df8168
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-24 10:29:35 -0500

    Set up data map for slides

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowseContainer } from '../containers/browse';
 import { useContent } from '../hooks';
+import selectionMap from '../utils/selection-map';
 
 export default function Browse() {
     const { series } = useContent('series');
     const { films } = useContent('films');
+    const slides = selectionMap({ series, films });
 
-    return <BrowseContainer />
+    return <BrowseContainer slides={slides} />;
 }
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { default as selectionMap } from './selection-map';
