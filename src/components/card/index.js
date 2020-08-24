39f3a9b548edc9e57cb35aacd39d30e5e053eb68
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-24 11:19:34 -0500

    Build out Card and its data mapping in Browse

---
+++ src/components/card/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {} from './styles/card';
+
+export default function Card() {
+    return ;
+}
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -5,11 +5,12 @@ import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
-export function BrowseContainer() {
+export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
     const [category, setCategory] = useState({});
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
     const { firebase } = useContext(FirebaseContext);
 
@@ -23,6 +24,10 @@ export function BrowseContainer() {
             setLoading(false)
         }, 10000)
     })
+
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    }, [slides, category])
 
     return profile.displayName ? (
         <>
@@ -61,17 +66,36 @@ export function BrowseContainer() {
                     </Header.Group>
                 </Header.Frame>
 
-            <Header.Feature>
-                <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
-                <Header.Text>
-                    Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-                    City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-                    futile attempt to feel like he's part of the world around him.
-                </Header.Text>
-                <Header.PlayButton>Play</Header.PlayButton>
-            </Header.Feature>
-            
+                <Header.Feature>
+                    <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                    <Header.Text>
+                        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+                        City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+                        futile attempt to feel like he's part of the world around him.
+                    </Header.Text>
+                    <Header.PlayButton>Play</Header.PlayButton>
+                </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map((slideItem) => {
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                    </Card>
+                })}
+            </Card.Group>
+
             <FooterContainer />
         </>)
         : (<SelectProfileContainer user={user} setProfile={setProfile} />);
